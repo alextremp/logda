016ce020f92117088d217df87f29d9a28172ef8a
Alex Castells <alextremp@hotmail.com>
2019-12-29 01:37:26 +0100

    feat(coverage) add nyc framework

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# logda
+# logda [![NPM Module](https://img.shields.io/npm/v/logda.svg)](https://www.npmjs.com/package/logda)
 
 [![Build Status](https://travis-ci.org/alextremp/logda.svg?branch=master)](https://travis-ci.org/alextremp/logda)
 [![codecov](https://codecov.io/gh/alextremp/logda/branch/master/graph/badge.svg)](https://codecov.io/gh/alextremp/logda)
+[![Maintainability](https://api.codeclimate.com/v1/badges/53000060cbda73bad602/maintainability)](https://codeclimate.com/github/alextremp/logda/maintainability)
 
--- src/test/indexTest.js
+++ src/test/indexTest.js
@@ -5,22 +5,49 @@ import {logda, setLogdaLevel} from './../main/index'
 import {LEVEL} from '../main/logger/Level'
 
 describe('index', () => {
-  const infoSpy = sinon.spy(console, 'info')
-  const warnSpy = sinon.spy(console, 'warn')
+  let consoleInfo
+  let consoleWarn
+  let consoleError
+  let infoSpy
+  let warnSpy
+  before(() => {
+    consoleInfo = console.info
+    consoleWarn = console.warn
+    consoleError = console.error
+    console.info = () => null
+    console.warn = () => null
+    console.error = () => null
+    infoSpy = sinon.spy(console, 'info')
+    warnSpy = sinon.spy(console, 'warn')
+  })
+  after(() => {
+    console.info = consoleInfo
+    console.warn = consoleWarn
+    console.error = consoleError
+  })
   beforeEach(() => {
+    setLogdaLevel('info')
     infoSpy.resetHistory()
     warnSpy.resetHistory()
   })
 
   it('should be error level by default', () => {
+    setLogdaLevel()
     const log = logda()
     expect(log.level).to.equal(LEVEL.error.label)
+    log.trace(() => 'test')
+    log.debug(() => 'test')
     log.info(() => 'test')
     expect(infoSpy.callCount).to.equal(0)
   })
 
+  it('should be error level if an invalid level is passed', () => {
+    setLogdaLevel('whatever')
+    const log = logda()
+    expect(log.level).to.equal(LEVEL.error.label)
+  })
+
   it('should get the global log level', () => {
-    setLogdaLevel('info')
     const log = logda()
     expect(log.level).to.equal(LEVEL.info.label)
     log.info(() => 'test')
@@ -28,47 +55,44 @@ describe('index', () => {
   })
 
   it('should be able to create a tagged logger', () => {
-    setLogdaLevel('info')
     const log = logda('Test')
     log.info(() => 'hello')
-    expect(infoSpy.getCall(0).args).deep.equal(['[INFO]>Test>', 'hello'])
+    expect(infoSpy.getCall(0).args).deep.equal(['[INFO] Test>', 'hello'])
   })
 
   it('should be able to create sub tagged loggers', () => {
-    setLogdaLevel('info')
     const appLogger = logda('Test')
     const log1 = appLogger.logger('Component1')
     const log2 = appLogger.logger('Component2')
+    const log3 = appLogger.logger()
 
     log1.info(() => 'from 1')
     log2.info(() => 'from 2')
+    log3.info(() => 'from 3')
 
     expect(infoSpy.getCall(0).args).deep.equal([
-      '[INFO]>Test|Component1>',
+      '[INFO] Test|Component1>',
       'from 1'
     ])
     expect(infoSpy.getCall(1).args).deep.equal([
-      '[INFO]>Test|Component2>',
+      '[INFO] Test|Component2>',
       'from 2'
     ])
+    expect(infoSpy.getCall(2).args).deep.equal(['[INFO] Test>', 'from 3'])
   })
 
   it('should not fail if provider is not a function', () => {
-    setLogdaLevel('info')
     const log = logda()
-    log.info('not a function')
-    expect(warnSpy.callCount).to.equal(1)
-    expect(infoSpy.callCount).to.equal(0)
+    expect(() => log.error('not a function')).to.not.throw()
   })
 
   it('should not fail if provider throws an exception', () => {
-    setLogdaLevel('info')
     const log = logda()
-    log.info(() => {
-      throw new Error('Owwww')
-    })
-    expect(warnSpy.callCount).to.equal(1)
-    expect(infoSpy.callCount).to.equal(0)
+    expect(() =>
+      log.error(() => {
+        throw new Error('Owwww')
+      })
+    ).to.not.throw()
   })
 
   it('should override the level with the local storage level', () => {
